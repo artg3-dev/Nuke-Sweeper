d4859d31bb40b2c1feed7bda368d68ddd63a366f
artg.dev <artg3.dev@gmail.com>
2020-02-19 18:40:26 -0500

    Create NodeButton.java

---
+++ src/nukesweeper/GUI/NodeButton.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nukesweeper.GUI;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Objects;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.border.BevelBorder;
+import nukesweeper.Engine.Node;
+
+/**
+ *
+ * @author Art Garcia (artg3.dev@gmail.com)
+ */
+public class NodeButton extends JButton implements MouseListener, ActionListener{
+    private final Color backgroundColor = Color.decode("#000000");
+    private final Color checkedBackgroundColor = Color.decode("#2D2D2D");
+    private final Color borderDark = Color.decode("#1D1D1D");
+    private final Color borderLight = Color.decode("#4D4D4D");
+    
+    private final Node node;
+
+    public NodeButton(Node node) {
+        super();
+        super.setContentAreaFilled(false);
+        this.node = node;
+        format();
+    }
+
+    private void format() {
+        addMouseListener(this);
+        addActionListener(this);
+        setFocusable(false);
+        setBackground(backgroundColor);
+        setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED, borderLight, borderDark));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        if (getModel().isPressed()) {
+//            g.setColor(Color.pink);
+            g.setColor(getBackground());
+        } else if (getModel().isRollover()) {
+            g.setColor(getBackground());
+        } else {
+            g.setColor(getBackground());
+        }
+        g.fillRect(0, 0, getWidth(), getHeight());
+        super.paintComponent(g);
+    }
+
+    @Override
+    public void setContentAreaFilled(boolean b) {
+    }
+
+    @Override
+    public boolean equals(Object inQuestion) {
+        if (inQuestion == null) {
+            return false;
+        }
+
+        if (getClass() != inQuestion.getClass()) {
+            return false;
+        }
+
+        NodeButton compared = (NodeButton) inQuestion;
+        return this.node.equals(compared.node);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 13 * hash + Objects.hashCode(this.node);
+        return hash;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON3) {
+            System.out.println("RIGHT CLICK");
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        setBackground(checkedBackgroundColor);
+        setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED, borderLight, borderDark));
+    }
+}
